a08824fe5dfafed1befef6d4813c0cc725e42189
miguel <miguel@desarrolloMAM>
2015-09-24 17:50:57 +0200

    nuevos controladores

---
+++ protected/config/database.php
@@ -0,0 +1,13 @@
+<?php
+// This is the database connection configuration.
+return array(
+	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	// uncomment the following lines to use a MySQL database
+	
+	'connectionString' => 'mysql:host=localhost;dbname=preventa',
+	'emulatePrepare' => true,
+	'username' => 'root',
+	'password' => 'VCMaria2015*',
+	'charset' => 'utf8',
+	
+);
