28fbc15a2e373b7422dae1770e8e9e5496c9e210
miguel <miguel@desarrolloMAM>
2015-10-05 13:22:17 +0200

    nuevo

--- protected/config/database.php
+++ protected/config/database.php
@@ -4,10 +4,10 @@ return array(
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	
-	'connectionString' => 'mysql:host=localhost;dbname=preventa',
+	'connectionString' => 'xxx',
 	'emulatePrepare' => true,
 	'username' => 'root',
-	'password' => 'VCMaria2015*',
+	'password' => 'xx',
 	'charset' => 'utf8',
 	
 );
